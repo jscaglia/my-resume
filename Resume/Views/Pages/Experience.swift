49c1e49889951db03179718be5c886f33a96178b
jscaglia <jonatanscaglia@gmail.com>
2022-09-19 21:06:20 -0300

    Update work experience

--- Resume/Views/Pages/Experience.swift
+++ Resume/Views/Pages/Experience.swift
@@ -16,8 +16,25 @@ struct Experience: View{
                     Rectangle().foregroundColor(.clear).border(.white, width: 2).shadow(color: .white, radius: 5)
                     VStack{
                         VStack{
+                            Text("FullStack Developer").font(.system(size: 16)).multilineTextAlignment(.center).padding(.horizontal)
+                            Text("Freelance | October 2021 - Currently").font(.system(size: 16)).multilineTextAlignment(.center).padding(.horizontal)
+                        }
+                        
+                        Text("Analysis, development and maintenance of the different applications as a member of the BackEnd & FrontEnd development teams.").font(.system(size: 12)).padding().multilineTextAlignment(.center).frame(height: 130)
+                        
+                        HStack{
+                            Image("FrontEnd").resizable().aspectRatio(contentMode: .fit).frame(width: 50, height: 50, alignment: .bottom)
+                            Image("BackEnd").resizable().aspectRatio(contentMode: .fit).frame(width: 50, height: 50, alignment: .bottom)
+                        }.padding(.bottom)
+                    }.frame(width: 300, height: 300)
+                }
+                
+                ZStack{
+                    Rectangle().foregroundColor(.clear).border(.white, width: 2).shadow(color: .white, radius: 5)
+                    VStack{
+                        VStack{
                             Text("FrontEnd Developer").font(.system(size: 16)).multilineTextAlignment(.center).padding(.horizontal)
-                            Text("Satoshitango | October 2021").font(.system(size: 16)).multilineTextAlignment(.center).padding(.horizontal)
+                            Text("Satoshitango | October 2021 - Currently").font(.system(size: 16)).multilineTextAlignment(.center).padding(.horizontal)
                         }
                         
                         Text("Analysis, development and maintenance of the Satoshitango application in the different available platforms.").font(.system(size: 12)).padding().multilineTextAlignment(.center).frame(height: 130)
